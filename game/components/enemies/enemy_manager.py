468cf9f27dab158b3f0d6283aed78ed1660449f7
JuliethGiraldo <darcy.giraldo@fundacion-jala.org>
2023-07-17 15:08:49 -0500

    SECOND_HOMEWORK

--- game/components/enemies/enemy_manager.py
+++ game/components/enemies/enemy_manager.py
@@ -1,4 +1,5 @@
 from game.components.enemies.enemy import Enemy
+from game.components.enemies.sapphire import Sapphire
 
 class EnemyManager:
     def __init__(self):
@@ -9,12 +10,19 @@ class EnemyManager:
 
         for enemy in self.enemies:
             enemy.update(self.enemies)
+        for sapphire in self.enemies:
+            sapphire.update(self.enemies)
 
     def draw(self, screen):
         for enemy in self.enemies:
             enemy.draw(screen)
+        for sapphire in self.enemies:
+            sapphire.draw(screen)
 
     def add_enemy(self):
         if len(self.enemies) < 1:
             enemy = Enemy()
             self.enemies.append(enemy)
+        if len(self.enemies) < 2:
+            sapphire = Sapphire()
+            self.enemies.append(sapphire)
